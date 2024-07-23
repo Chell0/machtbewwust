953ca3dfd593d0407ad1ee8bb0d954ee3a938c71
chell0 <machelgabriel@gmail.com>
2024-07-24 00:06:50 +0300

    Add BlogPost interface

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -6,3 +6,17 @@ export interface ThemenCard {
 	currentSlug: string;
 	titleImage: any;
 }
+
+// BlogPost
+export interface ThemenPost {
+	currentSlug: string;
+	title: string;
+	date: string;
+	titleImage: any;
+	content: any;
+	tags: string[];
+	authors: {
+		name: string;
+		specialty: string;
+	}[];
+}
