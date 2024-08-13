4a872e44351362bb7f6ac5878eae5fc20cd6d464
Machel Odhiambo <machelgabriel@gmail.com>
2024-08-14 01:42:00 +0300

    Update About Section Text

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,15 +2,7 @@ export default function About() {
     return (
         <div className="bg-white p-5 rounded-lg shadow-lg mx-auto w-full max-w-[1017px]">
             <p className="text-gray-600 text-md">
-                Willkommen auf dem Blog MachtBewusst-Hamburg! Hier dreht sich alles um
-                machtkritische und dekoloniale Perspektiven der Hamburger
-                Zivilgesellschaft. Wir möchten das Engagement und die Projekte im
-                Dekolonisierungsprozess in Hamburg hervorheben und euch mit
-                interessanten Beiträgen, Fachwissen und Erfahrungen inspirieren. Egal
-                ob ihr bereits aktiv oder einfach neugierig seid, hier findet ihr eine
-                Datenbank mit Akteuren:innen, einen Veranstaltungskalender und
-                spannende Materialien zur Thematik. Schaut vorbei, informiert euch und
-                lasst euch von neuen Gedanken anregen.
+                Willkommen auf dem Blog MachtBewusst-Hamburg! Hier dreht sich alles um machtkritische und dekoloniale Perspektiven der Hamburger Zivilgesellschaft. Wir möchten das Engagement und die Projekte im Dekolonisierungsprozess in Hamburg hervorheben und Euch mit interessanten Beiträgen, Fachwissen und Erfahrungen inspirieren. Egal ob, Ihr bereits aktiv oder einfach neugierig seid, hier findet Ihr eine Datenbank mit Akteuren:innen, einen Veranstaltungskalender und spannende Materialien zur Thematik. Schaut vorbei, informiert Euch und lasst Euch von neuen Gedanken anregen.
             </p>
         </div>
     );
